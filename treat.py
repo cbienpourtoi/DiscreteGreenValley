d31fa577d6a6aca0882bba617ce824261b5a7baa
Loic Le Tiran <loic.le-tiran@obspm.fr>
2015-05-05 19:01:53 -0300

    Improved readability

--- treat.py
+++ treat.py
@@ -49,23 +49,14 @@ from astropy.table import Table
 from astropy.io import ascii
 #import bz2
 import subprocess
-
-############
-### Init ###
-############
+from scipy.spatial.distance import cdist
 
 
-# Initial files (sdss for R-band, galex for NUV band)
-#GalexFile = "galex/MAST_2015-04-29T2046/GALEX/6371021344036880384/AIS_3_sg33-nd-int.fits"
-#SDSSFile = "sdss/frame-r-004682-4-0092.fits"
+#######################
+# Galaxy initial data #
+#######################
 
-
-
-
-######################
-# Galaxy coordinates #
-######################
-# galex data from:
+# Download galex data from:
 # http://galex.stsci.edu/GR6/?page=mastform
 
 galaxies = Table.read("galaxy_list.txt", format="ascii")
@@ -86,16 +77,7 @@ for galaxy in galaxies:
 	out = out+name+"/"
 	if not os.path.exists(out):
 		os.mkdir(out)
-	
-	"""
-	# NGC6125 E
-	RA = 244.7958
-	Dec = 57.9842
 
-	# NGC4185 S
-	#RA = 183.3417
-	#Dec = 28.5103
-	"""
 
 
 	###################################################
@@ -144,6 +126,15 @@ for galaxy in galaxies:
 	###################################################
 	# Downloads directly from SDSS using coordinates: #
 	###################################################
+
+	# TODO:
+	# Or better, make a mosaic !
+	# http://data.sdss3.org/mosaic-server/mosaic?onlyprimary=False&pixelscale=0.396&ra=155.86248&filters=r&dec=19.89849&size=0.3
+	# crap, automatic mosaic does work
+	
+	
+	urllib.urlretrieve("http://data.sdss3.org/mosaic-server/mosaic?onlyprimary=False&pixelscale=0.396&ra=155.86248&filters=r&dec=19.89849&size=0.3", "test.tar")
+	urllib.urlcleanup()
 
 	sdss_table_file = out+"sdsstable.csv"
 	urllib.urlretrieve("http://skyserver.sdss.org/dr10/en/tools/search/x_radial.aspx?ra="+str(RA)+"&dec="+str(Dec)+"&radius=0.2&format=csv&limit=20", sdss_table_file)
@@ -233,7 +224,6 @@ for galaxy in galaxies:
 	tRfile = out+"thumbR.fits"
 	tNUVfile = out+"thumbNUV.fits"
 
-
 	subprocess.call(["mSubimage", sdss_lowres_file, tRfile, str(RA), str(Dec), "0.1"])
 	subprocess.call(["mSubimage", galex_projected, tNUVfile, str(RA), str(Dec), "0.1"])
 
@@ -255,145 +245,3 @@ for galaxy in galaxies:
 	
 	#sys.exit()
 
-
-
-
-
-
-
-
-
-
-
-
-################
-#### Drafts ####
-################
-
-
-"""
-
-aplpy.make_rgb_cube([sdss_lowres_file, sdss_lowres_file, galex_projected], 'rgb.fits')
-
-aplpy.make_rgb_image('rgb.fits', 'rgb.png', vmin_r=20, vmax_r=400, vmin_g=0, vmax_g=150, vmin_b=-2,vmax_b=50)
-
-fig = aplpy.FITSFigure('rgb.fits')
-
-fig.show_rgb('rgb.png')
-
-# Add contours
-fig.show_contour('sc.fits', cmap='gist_heat', levels=[0.2,0.4,0.6,0.8,1.0])
-
-# Overlay a grid
-fig.add_grid()
-fig.grid.set_alpha(0.5)
-
-# Save image
-fig.save('plot.png')
-                     
-"""
-"""
-img = np.zeros((sdss_lr_image.shape[0], sdss_lr_image.shape[1], 3), dtype=float)
-img[:,:,0] = img_scale.sqrt(sdss_lr_image, scale_min=0, scale_max=10000)
-img[:,:,1] = img_scale.sqrt(galex_proj_image, scale_min=0, scale_max=10000)
-img[:,:,2] = img_scale.sqrt(last_color, scale_min=0, scale_max=10000)
-"""
-
-
-
-"""
-RA = 244.7958
-Dec = 57.9842
-
-GalexFile = "galex/MAST_2015-04-29T2046/GALEX/6371021344036880384/AIS_3_sg33-nd-int.fits"
-outgalex = "galex_stamp_l.fits"
-
-SDSSFile = "sdss/frame-r-004682-4-0092.fits"
-outsdss = "sdss_stamp_l.fits"
-
-cutout.cutout(GalexFile, RA, Dec, 0.05, 0.05, units='wcs', outfile=outgalex, coordsys='celestial')
-cutout.cutout(SDSSFile, RA, Dec, 0.04, 0.04, units='wcs', outfile=outsdss, coordsys='celestial')
-
-
-images_to_align = sorted(glob.glob(outgalex))
-ref_image = outsdss
-
-images_to_align = sorted(glob.glob(outsdss))
-ref_image = outgalex
-
-#images_to_align = sorted(glob.glob("sdss/frame-r-004682-4-0092.fits"))
-#ref_image = "galex/MAST_2015-04-29T2046/GALEX/6371021344036880384/AIS_3_sg33-nd-int.fits"
-
-identifications = alipy.ident.run(ref_image, images_to_align, visu=False, hdu=1)
-# That's it !
-# Put visu=True to get visualizations in form of png files (nice but much slower)
-# On multi-extension data, you will want to specify the hdu (see API doc).
-
-# The output is a list of Identification objects, which contain the transforms :
-for id in identifications: # list of the same length as images_to_align.
-        if id.ok == True: # i.e., if it worked
-
-                print "%20s : %20s, flux ratio %.2f" % (id.ukn.name, id.trans, id.medfluxratio)
-                # id.trans is a alipy.star.SimpleTransform object. Instead of printing it out as a string,
-                # you can directly access its parameters :
-                #print id.trans.v # the raw data, [r*cos(theta)  r*sin(theta)  r*shift_x  r*shift_y]
-                #print id.trans.matrixform()
-                #print id.trans.inverse() # this returns a new SimpleTransform object
-
-        else:
-                print "%20s : no transformation found !" % (id.ukn.name)
-
-
-# Minimal example of how to align images :
-
-outputshape = alipy.align.shape(ref_image)
-# This is simply a tuple (width, height)... you could specify any other shape.
-
-for id in identifications:
-        if id.ok == True:
-
-                # Variant 1, using only scipy and the simple affine transorm :
-                alipy.align.affineremap(id.ukn.filepath, id.trans, shape=outputshape, makepng=True)
-
-                # Variant 2, using geomap/gregister, correcting also for distortions :
-                #alipy.align.irafalign(id.ukn.filepath, id.uknmatchstars, id.refmatchstars, shape=outputshape, makepng=False)
-                # id.uknmatchstars and id.refmatchstars are simply lists of corresponding Star objects.
-
-                # By default, the aligned images are written into a directory "alipy_out".
-
-
-
-
-
-
-"""
-"""
-import aplpy
-
-fileR = 'sdss/frame-r-004682-4-0092.fits'
-#fileNUV = "galex/MAST_2015-04-29T2046/GALEX/6371021344036880384/AIS_3_sg33-fd-int.fits.gz"
-
-
-
-
-f = aplpy.FITSFigure(fileR)
-#f.show_grayscale()
-
-
-"""
-
-"""
-
-hdulist = fits.open(fileR)
-R = hdulist[1].data
-if hdulist[0].header["FILTER"] != 'r': print "Error: wrong filter for SDSS"
-hdulist.close()
-
-hdulist = fits.open(fileR)
-
-imgplot = plt.imshow(hdulist[0].data)
-#imgplot.set_cmap('binary_r')
-
-#plt.show()
-#hdulist.close()
-"""
